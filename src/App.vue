9018293681f4171b1fc6db1baf716bed39f667a6
Tim <tim.burg@particulate.me>
2019-11-15 18:02:33 +0100

    First steps

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <p>Message: {{ message }}</p>
+    <button v-on:click="reverseMessage">
+      Reverse Message
+    </button>
+    <input :value="message" @input="changeMessage"/>
+
+    <h3 class="padding-top-40">Todo list</h3>
+    <Todo/>
+  </div>
+</template>
+
+<script lang="ts">
+import { mapGetters, mapActions } from 'vuex';
+import { Component, Vue } from 'vue-property-decorator';
+import Todo from './components/Todo.vue';
+
+@Component({
+  components: {
+    Todo,
+  },
+  computed: {
+    message() {
+      return this.$store.state.message;
+    },
+  },
+  methods: {
+    ...mapActions([
+      'changeMessage',
+      'reverseMessage',
+    ]),
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+@for $i from 1 to 40 {
+  $i: $i+1;
+  .padding-top-#{$i} {
+    padding-top: $i *px;
+  }
+}
+
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <ol>
+    <TodoItem/>
+  </ol>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class TodoItem extends Vue {}
+</script>
+
+<style scoped lang="scss">
+</style>
